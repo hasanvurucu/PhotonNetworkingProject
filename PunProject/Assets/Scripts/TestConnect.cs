e442ae10f4489aabfb78f64604222a9065f1435e
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2020-10-09 00:18:26 +0300

    Initial Commit
    Very first commit of this project.

---
+++ PunProject/Assets/Scripts/TestConnect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        print("Connecting to server...");
+        PhotonNetwork.NickName = MasterManager.GameSettings.NickName;
+        PhotonNetwork.GameVersion = MasterManager.GameSettings.GameVersion;
+        PhotonNetwork.GameVersion = "0.0.1"; //When the game version is updated within the game, this will prevent players to play across different versions
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to server!");
+        print(PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        print("Disconnected from server for reason: " + cause.ToString());
+    }
+}
