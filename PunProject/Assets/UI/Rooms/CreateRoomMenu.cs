61b3f6bf5e37867831067bee32d9d2e0a2dd028d
hasanvurucu <47665574+hasanvurucu@users.noreply.github.com>
2020-10-15 22:16:31 +0300

    Some Room Operations
    Room creation and listing

---
+++ PunProject/Assets/UI/Rooms/CreateRoomMenu.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class CreateRoomMenu : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private Text roomName;
+
+    public void OnClick_CreateRoom()
+    {
+        if (!PhotonNetwork.IsConnected)
+            return;
+        //CreateRoom
+        //JoinOrCreateRoom
+        RoomOptions options = new RoomOptions();
+        options.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(roomName.text, options, TypedLobby.Default);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created room successfully.");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Room creation failed.");
+    }
+}
